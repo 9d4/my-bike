2c2542c3f0023f1d9ab592b684218046dff7ed1e
Dimas Ananda <traperwaze@gmail.com>
2022-05-10 08:47:39 +0700

    init
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

---
+++ src/secret.example.h
@@ -0,0 +1,10 @@
+#ifndef SECRET_H
+#define SECRET_H
+
+const char* ssid = "traperbike";
+const char* password = "123456789";
+
+const char* http_username = "traper";
+const char* http_password = "sutelo001";
+
+#endif
